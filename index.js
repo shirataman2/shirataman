c50a3879086cd8f474ee55fb9862ac52da4847ca
shirataman2 <nspacenonono@gmail.com>
2025-11-23 04:28:29 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
-const { 
-    joinVoiceChannel,
-    createAudioPlayer,
-    createAudioResource,
-    AudioPlayerStatus 
-} = require("@discordjs/voice");
-const play = require("play-dl");
+const { Client, GatewayIntentBits } = require('discord.js');
+const DeepL = require('deepl-node');
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-client.once("ready", () => {
+// DeepL クライアント
+const deepl = new DeepL.Translator(process.env.DEEPL_API_KEY);
+
+client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("messageCreate", async (message) => {
-    if (!message.content.startsWith("!play")) return;
+client.on('messageCreate', async (msg) => {
+    if (!msg.content.startsWith('!tr ')) return;
 
-    const args = message.content.split(" ");
-    const url = args[1];
+    const text = msg.content.replace('!tr ', '');
 
-    if (!url) {
-        return message.reply("YouTubeのURLを指定してね！");
+    try {
+        const result = await deepl.translateText(text, null, 'EN-US');
+        msg.reply(result.text);
+    } catch (err) {
+        console.error(err);
+        msg.reply('翻訳中にエラーが起きました');
     }
-
-    if (!message.member.voice.channel) {
-        return message.reply("先にボイスチャンネルに入ってね！");
-    }
-
-    const vc = message.member.voice.channel;
-
-    // VCへ接続
-    const connection = joinVoiceChannel({
-        channelId: vc.id,
-        guildId: vc.guild.id,
-        adapterCreator: vc.guild.voiceAdapterCreator,
-    });
-
-    // YouTube音源取得
-    const stream = await play.stream(url);
-    const resource = createAudioResource(stream.stream, {
-        inputType: stream.type
-    });
-
-    const player = createAudioPlayer();
-    connection.subscribe(player);
-
-    player.play(resource);
-
-    player.on(AudioPlayerStatus.Idle, () => {
-        message.channel.send("再生が終了しました！");
-    });
-
-    message.reply("? 再生を開始したよ！");
 });
 
 client.login(process.env.DISCORD_TOKEN);
